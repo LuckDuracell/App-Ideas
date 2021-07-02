6528a6c7ef9642584086f36fc195dddbdf9e2066
LuckDuracell <lukedru@yahoo.com>
2021-07-02 12:22:27 -0700

    Initial Commit

---
+++ Shared/App_IdeasApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_IdeasApp.swift
+//  Shared
+//
+//  Created by Luke Drushell on 7/2/21.
+//
+
+import SwiftUI
+
+@main
+struct App_IdeasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
